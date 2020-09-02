a5a1dd47202f2762089482a84bd408e59cef6e39
jim-fuller <2007H3@outlook.comcom>
2020-09-02 18:58:20 -0500

    update jade template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Billing' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
 });
 
 module.exports = router;
